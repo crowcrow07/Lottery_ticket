9fefeb6fdcb3f41b7dd5681dba273e190efaa821
crowcrow07 <skathd3402@gmail.com>
2023-12-25 20:59:13 +0900

    feat : 복권 번호 화면에 나열 컴포넌트 제작 완료

--- client/src/components/Main/LotteryNumberDisplay.jsx
+++ client/src/components/Main/LotteryNumberDisplay.jsx
@@ -1,5 +1,50 @@
 import styles from "./LotteryNumberDisplay.module.css";
 
-export default function LotteryNumberDisplay() {
-  return <div className={styles.container}>복권번호를 출력하는 곳</div>;
+export default function LotteryNumberDisplay({ data }) {
+  if (!data) {
+    return <div>서버와 통신할수없습니다.</div>;
+  }
+  const {
+    bnusNo,
+    drwNo,
+    drwNoDate,
+    drwtNo1,
+    drwtNo2,
+    drwtNo3,
+    drwtNo4,
+    drwtNo5,
+    drwtNo6,
+  } = data;
+
+  const lotteryNumberArr = [];
+
+  lotteryNumberArr.push(
+    drwtNo1,
+    drwtNo2,
+    drwtNo3,
+    drwtNo4,
+    drwtNo5,
+    drwtNo6,
+    bnusNo
+  );
+
+  console.log(lotteryNumberArr);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles["info-container"]}>
+        <div>{`${drwNo} 회차`}</div>
+        <div>{`${drwNoDate}에 추첨됨`}</div>
+      </div>
+      <div className={styles["lottery-number-container"]}>
+        {lotteryNumberArr.map((v) => {
+          return (
+            <div className={styles["lottery-number"]} key={v}>
+              {v}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
