bcfdcee093c764c332c4bc0a850a26e1fc1ea6ef
crowcrow07 <skathd3402@gmail.com>
2023-12-25 16:08:15 +0900

    feat : 글 문구 수정

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
     <header>
       <div className={styles.banner}>
         <div className={styles.lnb}>
-          <a href="#none">깃허브</a>
-          <a href="#none">기술 블로그</a>
-          <a href="#none">인스타?</a>
+          <a href="#none">제작자의 깃허브</a>
+          <a href="#none">제작자의 블로그</a>
+          {/* <a href="#none">제작자의 인스타</a> */}
         </div>
       </div>
     </header>
