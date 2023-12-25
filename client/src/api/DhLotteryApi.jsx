34767cf93877a79b3001ad68435d79aa45ef7035
crowcrow07 <skathd3402@gmail.com>
2023-12-25 16:07:55 +0900

    feat : node server로 fetch 요청을 보내야하는 url을 전달

--- client/src/api/DhLotteryApi.jsx
+++ client/src/api/DhLotteryApi.jsx
@@ -1,9 +1,22 @@
-export default function DhLotteryApi(number) {
-  fetch(
-    `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+${number}`
-  )
-    .then((res) => {
-      res.json();
-    })
-    .then((json) => console.log(json));
+const MY_SERVER_URL = "http://localhost:4000/getData";
+
+export default async function DhLotteryApi(number) {
+  const LOTTERY_API_URL = `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${number}`;
+
+  try {
+    const response = await fetch(MY_SERVER_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: LOTTERY_API_URL,
+      }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
