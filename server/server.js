ef1e2dde5ede2f3f769c6a85c5bf90775b7e2933
crowcrow07 <skathd3402@gmail.com>
2023-12-25 16:05:05 +0900

    feat : node express 서버 구축, cors 회피를 위한 프록시 서버, 재사용성 고려

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 const port = 4000;
 
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/getData", async (req, res) => {
+  const requestData = req.body;
+
+  try {
+    const response = await fetch(requestData.url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
