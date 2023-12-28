68b6a02702c7d8cdd98539dbc517ec08d6755524
crowcrow07 <skathd3402@gmail.com>
2023-12-28 15:53:23 +0900

    feat : 숫자를 한글로 바꾸는 함수 적용

--- client/src/components/Main/LotteryNumberDisplay.jsx
+++ client/src/components/Main/LotteryNumberDisplay.jsx
@@ -1,6 +1,8 @@
+import numberToKorean from "../../util/numberToKorean";
+
 import styles from "./LotteryNumberDisplay.module.css";
 
-export default function LotteryNumberDisplay({ data }) {
+export default function LotteryNumberDisplay({ data, attempt }) {
   if (!data) {
     return <div>서버와 통신할수없습니다.</div>;
   }
@@ -28,13 +30,19 @@ export default function LotteryNumberDisplay({ data }) {
     bnusNo
   );
 
-  console.log(lotteryNumberArr);
-
   return (
     <div className={styles.container}>
       <div className={styles["info-container"]}>
-        <div>{`${drwNo} 회차`}</div>
-        <div>{`${drwNoDate}에 추첨됨`}</div>
+        <div>
+          {attempt
+            ? `${numberToKorean(attempt)}장을 사야 합니다.`
+            : `${drwNo} 회차`}
+        </div>
+        <div>
+          {attempt
+            ? `${numberToKorean(attempt * 1000)}원을 투자해야합니다.`
+            : `${drwNoDate}에 추첨됨`}
+        </div>
       </div>
       <div className={styles["lottery-number-container"]}>
         {lotteryNumberArr.map((v) => {
