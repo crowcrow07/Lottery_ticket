a15d2843938a4e2fd26a822c62b52c0f973f47b0
crowcrow07 <skathd3402@gmail.com>
2023-12-28 15:55:43 +0900

    fixed : 사용 안하는 web worker 삭제

--- client/src/util/util.worker.js
+++ client/src/util/util.worker.js
@@ -1,5 +1,3 @@
-import TestWorker from "../workers/test.worker?worker";
 import CheckWinningNumbersWorker from "../workers/checkWinningNumbers.worker?worker";
 
-export const testWorker = new TestWorker();
 export const checkWinningNumbersWorker = new CheckWinningNumbersWorker();
